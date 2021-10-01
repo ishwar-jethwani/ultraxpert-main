acc05dc1c2d885fdf4f6e9fa132251ea6bad6c48
wolfpackproject <86668915+wolfpackproject@users.noreply.github.com>
2021-10-01 18:36:58 +0530

    final

--- payment/urls.py
+++ payment/urls.py
@@ -2,6 +2,7 @@ from django.urls import path,include
 from .views import *
 urlpatterns = [
     path("pay/<str:order_id>/",ServicePaymentAPIView.as_view(),name="payment"),
-    path("subs_pay/<str:subs_id>/",PlanPaymentAPIView.as_view(),name="subs_payment")
+    path("subs_pay/<str:subs_id>/",PlanPaymentAPIView.as_view(),name="subs_payment"),
+    path("response/",GetResponse.as_view(),name="response")
     
 ]
