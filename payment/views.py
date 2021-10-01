4fc11ab052a7389d13ff72c8be6931a6e5f7b206
wolfpackproject <86668915+wolfpackproject@users.noreply.github.com>
2021-10-01 15:50:18 +0530

    final

--- payment/views.py
+++ payment/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.views.generic.base import RedirectView
 import razorpay
 from django.views.decorators.csrf import csrf_exempt
 from django.http import HttpResponseBadRequest
@@ -15,29 +16,15 @@ razorpay_client = razorpay.Client(
     auth=(RAZOR_KEY_ID,RAZOR_KEY_SECRET))
  
  
-class PaymentAPIView(APIView):
+class ServicePaymentAPIView(APIView):
     permission_classes = [IsAuthenticated]
-    def get(self,request):
+    def get(self,request,order_id):
         currency = 'INR'
-        type = request.data["type"]
-        if type == "Plan":
-            sub_id = request.data["subs_id"]
-            subscription = Subscriptions.objects.get(subs_id=sub_id)
-            amount = int(subscription.plan.plan_price)*100
-        else:
-            order_id = request.data["order_id"]
-            order = Order.objects.get(order_id=order_id)
-            amount = int(order.price)*100
-
-
-    
-        # Create a Razorpay Order
+        order = Order.objects.get(order_id=order_id)
+        amount = int(order.price)*100
         razorpay_order = razorpay_client.order.create(dict(amount=amount,
                                                         currency=currency,
                                                         payment_capture='0'))
-        print(razorpay_order)
-    
-        # order id of newly created order.
         razorpay_order_id = razorpay_order['id']
         callback_url = 'pay/'
     
@@ -48,9 +35,32 @@ class PaymentAPIView(APIView):
         context['razorpay_amount'] = amount
         context['currency'] = currency
         context['callback_url'] = callback_url
-        return Response(context,status=HTTP_201_CREATED)
+        return render(request,"payment.html")
 
     @csrf_exempt
     def post(self,request):
-        pass
+        payment_id = request.POST.get('razorpay_payment_id', '')
+        razorpay_order_id = request.POST.get('razorpay_order_id', '')
+        signature = request.POST.get('razorpay_signature', '')
+        params_dict = {
+            'razorpay_order_id': razorpay_order_id,
+            'razorpay_payment_id': payment_id,
+            'razorpay_signature': signature
+        }
+
+        result = razorpay_client.utility.verify_payment_signature(
+                params_dict)
+        if result is None:
+            amount = 20000  # Rs. 200
+            try:
+                razorpay_client.payment.capture(payment_id, amount)
+ 
+                    # render success page on successful caputre of payment
+                return render(request, 'paymentsuccess.html')
+            except:
+                    # if there is an error while capturing payment.
+                return render(request, 'paymentfail.html')
+        else:
+            return render(request, 'paymentfail.html')
+        
 
