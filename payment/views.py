80980fe97ea95a0f0308a2ad1a7494d7ae1756ba
wolfpackproject <86668915+wolfpackproject@users.noreply.github.com>
2021-10-01 11:39:54 +0530

    final

--- payment/views.py
+++ payment/views.py
@@ -6,7 +6,7 @@ from UltraExperts.constants import RAZOR_KEY_SECRET,RAZOR_KEY_ID
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.status import HTTP_201_CREATED
-from activity.models import Order
+from activity.models import Order, Subscriptions
  
 # authorize razorpay client with API Keys.
 razorpay_client = razorpay.Client(
@@ -16,9 +16,16 @@ razorpay_client = razorpay.Client(
 class PaymentAPIView(APIView):
     def get(self,request):
         currency = 'INR'
-        order_id = request.data["order_id"]
-        order = Order.objects.get(order_id=order_id)
-        amount = order.price
+        type = request.data["type"]
+        if type == "Plan":
+            sub_id = request.data["subs_id"]
+            subscription = Subscriptions.objects.get(subs_id=sub_id)
+            amount = subscription.plan.plan_price
+        else:
+            order_id = request.data["order_id"]
+            order = Order.objects.get(order_id=order_id)
+            amount = order.price
+
     
         # Create a Razorpay Order
         razorpay_order = razorpay_client.order.create(dict(amount=amount,
@@ -40,55 +47,38 @@ class PaymentAPIView(APIView):
 
     @csrf_exempt
     def post(self,request):
-        if request.method == "POST":
-            try:
-            
-                # get the required parameters from post request.
-                payment_id = request.POST.get('razorpay_payment_id', '')
-                razorpay_order_id = request.POST.get('razorpay_order_id', '')
-                signature = request.POST.get('razorpay_signature', '')
-                params_dict = {
-                    'razorpay_order_id': razorpay_order_id,
-                    'razorpay_payment_id': payment_id,
-                    'razorpay_signature': signature
-                }
-    
-                # verify the payment signature.
-                result = razorpay_client.utility.verify_payment_signature(
-                    params_dict)
-                if result is None:
-                    amount = 20000  # Rs. 200
-                    try:
-    
-                        # capture the payemt
-                        razorpay_client.payment.capture(payment_id, amount)
-    
-                        # render success page on successful caputre of payment
-                        return Response({
-                                "order_id":params_dict[razorpay_order_id],
-                                "payment_id":params_dict["razorpay_payment_id"],
-                                "status":"sucess"
-                            })
-                    except:
-    
-                        # if there is an error while capturing payment.
-                        return Response({
-                                "order_id":params_dict[razorpay_order_id],
-                                "payment_id":params_dict["razorpay_payment_id"],
-                                "status":"failed"
-                            })
-                else:
-    
-                    # if signature verification fails.
-                     return Response({
-                                "order_id":params_dict[razorpay_order_id],
-                                "payment_id":params_dict["razorpay_payment_id"],
-                                "status":"signature failed"
-                            })
-            except:
-    
-                # if we don't find the required parameters in POST data
-                return HttpResponseBadRequest()
-        else:
-        # if other than POST request is made.
+
+        try:
+            payment_id = request.POST.get('razorpay_payment_id', '')
+            razorpay_order_id = request.POST.get('razorpay_order_id', '')
+            signature = request.POST.get('razorpay_signature', '')
+            params_dict = {
+                'razorpay_order_id': razorpay_order_id,
+                'razorpay_payment_id': payment_id,
+                'razorpay_signature': signature
+            }
+            result = razorpay_client.utility.verify_payment_signature(
+                params_dict)
+            if result is None:
+                amount = 20000  # Rs. 200
+                try:
+                    razorpay_client.payment.capture(payment_id, amount)
+                    return Response({
+                            "order_id":params_dict[razorpay_order_id],
+                            "payment_id":params_dict["razorpay_payment_id"],
+                            "status":"sucess"
+                        })
+                except:
+                    return Response({
+                            "order_id":params_dict[razorpay_order_id],
+                            "payment_id":params_dict["razorpay_payment_id"],
+                            "status":"failed"
+                        })
+            else:
+                return Response({
+                        "order_id":params_dict[razorpay_order_id],
+                        "payment_id":params_dict["razorpay_payment_id"],
+                        "status":"signature failed"
+                    })
+        except:
             return HttpResponseBadRequest()
