428ec1dfb3b00a86b1a2b9b0a89f2ea4759f65c5
Rishabh_07 <sahuji2607@gmail.com>
2021-12-07 17:49:40 +0530

    meet

--- meet/views.py
+++ meet/views.py
@@ -2,5 +2,5 @@ from django.shortcuts import render
 import os
 # Create your views here.
 def meet(request):
-    context = {"API_KEY":os.environ.get("API_KEY")}
+    context = {"API_KEY":os.environ["API_KEY"]}
     return render(request,"meet.html",context)
