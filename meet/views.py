926fe27b7cc12d96116cbd57dd781a23831eabd5
Rishabh_07 <sahuji2607@gmail.com>
2021-12-07 17:56:22 +0530

    meet

--- meet/views.py
+++ meet/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
 import os
+from UltraExperts.constants import VIDEOSDK_API_KEY
 # Create your views here.
 def meet(request):
-    context = {"API_KEY":os.environ["API_KEY"]}
+    key = ""
+    if len(os.environ["API_KEY"]) == 0:
+        key = VIDEOSDK_API_KEY
+    else:
+        key = os.environ["API_KEY"]
+    context = {"API_KEY":key}
     return render(request,"meet.html",context)
