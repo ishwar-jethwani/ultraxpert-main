359800eac44fbbe703d13e91bb161eda2f5fe828
unknown <ishwar@ultracreation.tech>
2022-01-30 21:44:41 +0530

    final

--- events/urls.py
+++ events/urls.py
@@ -1,10 +1,8 @@
-from events.models import Event
-from django.urls import path,include
+from django.urls import path
 from .views import *
-from agora.views import Agora
-from .constant import AGORA_APP_ID, CHANNEL
+
 urlpatterns = [
-    path("google_event/<str:service_id>/", GoogleCalaenderAuthorizationAPIView.as_view(), name="google_calander"),
+    # path("google_event/<str:service_id>/", GoogleCalaenderAuthorizationAPIView.as_view(), name="google_calander"),
     path("event/", EventCreateAPIView.as_view(), name="event"),
 
 ]
