60bdc53dd22c9ff354ffb2c067718e66f49f699f
unknown <ishwar@ultracreation.tech>
2022-02-05 00:02:34 +0530

    final

--- meet/serializers.py
+++ meet/serializers.py
@@ -2,12 +2,14 @@ from UltraExperts.serializers import UserSerilizer
 from rest_framework.serializers import ModelSerializer
 from user.serializers import ServiceShowSerializer
 from events.serializers import EventReadSerializer
+from rest_framework import serializers
 
 from .models import *
 class MeetingSerializer(ModelSerializer):
     user= UserSerilizer()
     service = ServiceShowSerializer()
     event = EventReadSerializer()
+    date_time = serializers.DateTimeField(format="%c")
 
     class Meta:
         model = Meeting
