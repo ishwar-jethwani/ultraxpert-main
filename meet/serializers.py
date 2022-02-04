c0b37d617d30a79a1285f1655ae2b16913f1b23c
unknown <ishwar@ultracreation.tech>
2022-02-05 00:00:21 +0530

    final

--- meet/serializers.py
+++ meet/serializers.py
@@ -1,8 +1,14 @@
-from django.db import models
-from django.db.models import fields
+from UltraExperts.serializers import UserSerilizer
 from rest_framework.serializers import ModelSerializer
+from user.serializers import ServiceShowSerializer
+from events.serializers import EventReadSerializer
+
 from .models import *
 class MeetingSerializer(ModelSerializer):
+    user= UserSerilizer()
+    service = ServiceShowSerializer()
+    event = EventReadSerializer()
+
     class Meta:
         model = Meeting
         fields = "__all__"
