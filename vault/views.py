663b20cfd8f66c31188d30d99934653d2af8f645
unknown <ishwar@ultracreation.tech>
2022-02-11 19:52:23 +0530

    final

--- vault/views.py
+++ vault/views.py
@@ -5,11 +5,18 @@ from UltraExperts.constants import RAZOR_KEY_SECRET,RAZOR_KEY_ID,PAYMANT_BASE_UR
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from user.models import *
+import razorpay
+from rest_framework import status
+
+
+# client.virtual_account.create(data=DATA)
+# DATA should contain these keys
+#     receiver_types        : ['bank_account']
+#     description           : 'Random Description'
+#     customer_id(optional) : <CUSTOMER_ID>
 
 class CreateVirtualAccount(APIView):
-    endpoint2 = "virtual_accounts"
-    auth=HTTPBasicAuth(username=RAZOR_KEY_ID,password=RAZOR_KEY_SECRET)
-    url = PAYMANT_BASE_URL+endpoint2
+    client = razorpay.Client(auth=(RAZOR_KEY_ID,RAZOR_KEY_SECRET))
     permission_classes = [IsAuthenticated]
     def post(self,request):
         user = request.user
@@ -30,6 +37,6 @@ class CreateVirtualAccount(APIView):
                 }],
                 "customer_id":str(user.user_id) ,
         }
-        res = requests.request("POST",url=self.url,auth=self.auth,data=payload)
-        return Response({"data":res.json()})
+        res = self.client.virtual_account.create(data=payload)
+        return Response({"data":res.json()},status=status.HTTP_201_CREATED)
 
