4c7980694b97ae73a286dfddf95bb54f7e3952eb
unknown <ishwar@ultracreation.tech>
2022-02-13 18:01:32 +0530

    final

--- meet/models.py
+++ meet/models.py
@@ -25,9 +25,9 @@ class Meeting(models.Model):
 
 class MeetingTypeCount(models.Model):
     user = models.ForeignKey(User,on_delete=models.CASCADE)
-    meet_45 = models.PositiveIntegerField(blank=True,null=True)
-    meet_30 = models.PositiveIntegerField(blank=True,null=True)
-    meet_60 = models.PositiveIntegerField(blank=True,null=True)
+    meet_45 = models.IntegerField(blank=True,null=True)
+    meet_30 = models.IntegerField(blank=True,null=True)
+    meet_60 = models.IntegerField(blank=True,null=True)
     date_created = models.DateTimeField(auto_now_add=True,blank=True,null=True)
     date_updated = models.DateTimeField(auto_now=True,blank=True,null=True)
 
