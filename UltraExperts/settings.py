d3bf1c25975181fb9921e3618c79573fca0b9886
unknown <ishwar@ultracreation.tech>
2022-05-17 17:26:05 +0530

    chnage in settings

--- UltraExperts/settings.py
+++ UltraExperts/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = SECRET_KEY
 DEBUG = DEBUG
 #ALLOWED_HOSTS = ["*"]
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', '.ngrok.io','UltraExperts.herokuapp.com','ultraxpert-front.herokuapp.com','ultraxpert.com']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '.ngrok.io','UltraExperts.herokuapp.com','ultraxpert-front.herokuapp.com','ultraxpert.com','ultra-env.ap-south-1.elasticbeanstalk.com']
 
 if DEBUG == False:
     SECURE_SSL_REDIRECT = True
