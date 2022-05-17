3e8f8436cda85cd2261217e862db332e1ccaf7c6
unknown <ishwar@ultracreation.tech>
2022-05-17 18:32:57 +0530

    final

--- UltraExperts/settings.py
+++ UltraExperts/settings.py
@@ -167,11 +167,11 @@ CORS_ALLOW_HEADERS = [
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': config("DATABASE"),
-        'USER': config("USER"),
-        'PASSWORD': config("DB_PASSWORD"),
-        'HOST': config("HOST"),
-        'PORT': config("PORT"),
+        'NAME': os.environ["RDS_DB_NAME"],
+        'USER': os.environ["RDS_USERNAME"],
+        'PASSWORD': os.environ["RDS_PASSWORD"],
+        'HOST': os.environ["RDS_HOSTNAME"],
+        'PORT':  os.environ["RDS_PORTNAME"],
     }
 }
 
