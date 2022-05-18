dbbcbaace4a46fe432061888bae5f880c42e0410
unknown <ishwar@ultracreation.tech>
2022-05-18 16:31:36 +0530

    change in api

--- UltraExperts/settings.py
+++ UltraExperts/settings.py
@@ -60,8 +60,9 @@ INSTALLED_APPS = [
     # 'django_elasticsearch_dsl',
     # 'django_elasticsearch_dsl_drf',
     'django.contrib.sitemaps',
+    'background_task',
 ]
-
+BACKGROUND_TASK_RUN_ASYNC = True
 # social authenticaton application
 
 INSTALLED_APPS+=[
