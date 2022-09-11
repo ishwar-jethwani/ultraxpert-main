f2d3511a311cf601b9271105236f95af44ce22a4
ishwar <ishwar@zoconut.com>
2022-09-11 19:20:40 +0530

    final

--- UltraExperts/serializers.py
+++ UltraExperts/serializers.py
@@ -8,7 +8,7 @@ class CustomRegisterSerializer(RegisterSerializer):
     def get_cleaned_data(self):
         data_dict = super().get_cleaned_data()
         data_dict['reffered_by'] = self.validated_data.get('reffered_by', '')
-        data_dict["verification_satus"] = self.validated_data.get('verification_satus', False)
+        data_dict["verification_status"] = self.validated_data.get('verification_status', False)
         return data_dict
 
 
